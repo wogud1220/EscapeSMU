aa8060b2ad5a34cb7e6dc2b11a51b52859d302f3
윤재형 <yoonwogud@gmail.com>
2025-03-28 06:01:26 +0900

    700~900MB sizing

--- server/matcher.py
+++ server/matcher.py
@@ -19,9 +19,8 @@ def load_image(image_path):
     else:
         print(f"✅ 이미지 로드 성공: {image_path}")
     # ✅ 🔽 이미지 크기 줄이기 (크게 유지)
-    # image = cv2.resize(image, (2560, 1920))  # QHD 이상급
     # image = cv2.resize(image, (3264, 2448))  # 약 8MP (4:3 비율)
-
+    image = cv2.resize(image, (4032, 3024))  # 약 12MP (4:3 비율, 아이폰 기본)
     return image
 
 def compare_images(user_image_path):
