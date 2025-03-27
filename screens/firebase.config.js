3ec8e7fbdafe7381b5b51ac54c2ff029fb21f780
윤재형 <yoonwogud@gmail.com>
2025-03-28 00:00:12 +0900

    .

--- screens/firebase.config.js
+++ screens/firebase.config.js
@@ -1,32 +1,3 @@
-// import {initializeApp, getApps, getApp} from 'firebase/app';
-// import {initializeAuth, getReactNativePersistence} from 'firebase/auth';
-// import AsyncStorage from '@react-native-async-storage/async-storage';
-
-// const firebaseConfig = {
-//   apiKey: 'AIzaSyDJUzQ33Wll6dkgiujYGWhXytJRrhEpOEA',
-//   authDomain: 'escapesmu.firebaseapp.com',
-//   projectId: 'escapesmu',
-//   storageBucket: 'escapesmu.appspot.com',
-//   messagingSenderId: '109058266640',
-//   appId: '1:109058266640:android:71bdef512fe0e8b0051c95',
-// };
-
-// let app, auth;
-
-// if (getApps().length === 0) {
-//   app = initializeApp(firebaseConfig);
-//   auth = initializeAuth(app, {
-//     persistence: getReactNativePersistence(AsyncStorage),
-//   });
-// } else {
-//   app = getApp();
-//   auth = initializeAuth(app, {
-//     persistence: getReactNativePersistence(AsyncStorage),
-//   });
-// }
-
-// export {auth};
-
 import {initializeApp, getApps, getApp} from 'firebase/app';
 import {
   initializeAuth,
