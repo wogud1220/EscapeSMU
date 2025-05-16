7ed4165aab415e6ba3ca8c8fb9289dd1e4ed81ef
윤재형 <yoonwogud@gmail.com>
2025-05-16 13:46:29 +0900

    300미터로 계산

--- screens/Stage1Camera.tsx
+++ screens/Stage1Camera.tsx
@@ -146,7 +146,7 @@ const Stage1Camera = ({navigation}: {navigation: any}) => {
         );
         console.log('📍거리 차이:', distance.toFixed(2), 'm');
 
-        if (distance > 100) {
+        if (distance > 300) {
           Alert.alert('❌ 위치 제한', '조금 더 가까이 가주세요.');
           return;
         }
